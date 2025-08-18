ef673dfa971dadd00f78d5a0c1f5cce79ae59500
Eric Karlson <eric.karlson404+github@gmail.com>
2025-08-18 12:33:21 -0700

    Updated version number in Help.

--- CreationKitAuditTool/MainForm.h
+++ CreationKitAuditTool/MainForm.h
@@ -730,7 +730,7 @@ namespace CreationKitAuditTool {
 			L"Generates platform-specific ACHLIST files for packaging PC and XBox WEM files.\n\n" +
 			L"Generated ACHLIST files are stored in one's >Documents\\My Games\\Starfield\\CreationKitAuditTool< folder.\n\n" +
 			L"GitHub: https://github.com/ebkarlson404/CreationKitAuditTool\n\n" +
-			L"Version 0.9.0\n\n" +
+			L"Version 0.9.1\n\n" +
 			L"Copyright 2025, Eric Karlson\n\n" +
 			L"Distrbuted under the terms of the Apache License version 2.0, January 2004",
 			L"Creation Kit Audit Log Help",
