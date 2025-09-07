981076cae3a1b72d0f22da73a9ec4bed21a44027
Eric Karlson <eric.karlson404+github@gmail.com>
2025-09-07 07:10:47 -0700

    Bump Help version to 1.3.1

--- CreationKitAuditTool/MainForm.h
+++ CreationKitAuditTool/MainForm.h
@@ -1131,7 +1131,7 @@ private: System::Windows::Forms::ToolStripMenuItem^ notifyToolStripPauseItem;
 			L"Generates platform-specific ACHLIST files for packaging PC and XBox WEM files.\n\n" +
 			L"Generated ACHLIST files are stored in one's >Documents\\My Games\\Starfield\\CreationKitAuditTool< folder.\n\n" +
 			L"GitHub: " + githubUrl + L"\n\n" +
-			L"Version 1.3.0\n\n" +
+			L"Version 1.3.1\n\n" +
 			L"Copyright 2025, Eric Karlson\n\n" +
 			L"Distrbuted under the terms of the Apache License version 2.0, January 2004",
 			L"Creation Kit Audit Log Help",
